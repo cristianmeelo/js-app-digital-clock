c721cd96ae7aaf17ddeb0afe5165b10318a9e156
cristianmeelo <cristianmeelo@gmail.com>
2023-10-05 18:57:33 -0300

    :lipstick: styles: formatted code with prettier

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
+const hoursLabel = document.getElementById("hours");
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
 
-const hoursLabel = document.getElementById('hours');
-const minutesLabel = document.getElementById('minutes');
-const secondsLabel = document.getElementById('seconds');
+const clock = setInterval(function time() {
+  let dateToday = new Date();
+  let hours = dateToday.getHours();
+  let minutes = dateToday.getMinutes();
+  let seconds = dateToday.getSeconds();
 
-const clock = setInterval(function time () {
-    let dateToday = new Date();
-    let hours = dateToday.getHours();
-    let minutes = dateToday.getMinutes();
-    let seconds = dateToday.getSeconds();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
 
-    if (hours < 10){hours = '0' + hours;} 
-    if (minutes < 10){minutes = '0' + minutes;} 
-    if (seconds < 10){seconds = '0' + seconds;} 
-
-    hoursLabel.textContent = hours;
-    minutesLabel.textContent = minutes;
-    secondsLabel.textContent = seconds;
-
-})
-
-
-
+  hoursLabel.textContent = hours;
+  minutesLabel.textContent = minutes;
+  secondsLabel.textContent = seconds;
+});
